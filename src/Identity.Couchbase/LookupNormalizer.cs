8c2cc351456e0bcf21485d212413638d9834157d
Ross Merrigan <ross.merrigan@smartfocus.com>
2016-07-21 10:37:18 +0100

    init

---
+++ src/Identity.Couchbase/LookupNormalizer.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.AspNetCore.Identity;
+using System;
+
+namespace Identity.Couchbase
+{
+    public class LookupNormalizer : ILookupNormalizer
+    {
+        public string Normalize(string key)
+        {
+            return key.Normalize().ToLowerInvariant();
+        }
+    }
+}
---
+++ src/Identity.Couchbase/UserWrapper.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using Couchbase.Linq.Filters;
+
+namespace Identity.Couchbase
+{
+    [DocumentTypeFilter("user")]
+    class UserWrapper<TUser> where TUser : IUser
+    {
+        public UserWrapper()
+        {
+            Type = "user";
+        }
+
+        public UserWrapper(TUser user) : this()
+        {
+            User = user;
+        } 
+
+        public TUser User { get; set; }
+        public string Type { get; set; }
+    }
+}
